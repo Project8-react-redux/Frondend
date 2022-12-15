600ad2e94f8150a90bb590d5479b38519bc1ba74
Ramajardat <ramajardat6@gmail.com>
2022-12-14 16:45:18 -0800

    style

--- src/Components/generalComponents/Footer.jsx
+++ src/Components/generalComponents/Footer.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
-
+import { AiFillTwitterCircle} from 'react-icons/ai';
+import { GrFacebookOption } from "react-icons/gr";
+import { AiOutlineYoutube } from "react-icons/ai";
+import { AiFillLinkedin } from "react-icons/ai";
 import React from "react";
 import {
   MDBFooter,
@@ -17,72 +20,77 @@ export default function Footer() {
         <MDBRow>
           <MDBCol lg="6" md="12" className="mb-4 mb-md-0">
             <MDBCardImage src={logo} alt="avatar" height="80" />
+<div style={{marginTop:"30px",paddingLeft:"30px", fontSize:"20px"}}>
+
+            <a href={'/https://www.facebook.com/HAYYA-107115075582816'}> 
+                                    < GrFacebookOption/> </a>
+                                    <a href={'https://www.linkedin.com/company/hayya/?viewAsMember=true'}>
+                                    < AiFillLinkedin /></a>
+
+                                    <a href={'https://twitter.com/jardat_rama'}>
+                                    <AiFillTwitterCircle /> </a>
+                                    <a href={'https://www.youtube.com/@hayya3947'}>
+       <AiOutlineYoutube /></a>
+</div>
           </MDBCol>
 
-          <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
-            <h5 className="text-uppercase">Links</h5>
+          <MDBCol lg="3" md="12" className="mb-4 mb-md-0">
+            <h5 className="text-uppercase">Pages</h5>
 
             <ul className="list-unstyled mb-0">
               <li>
-                <Link href="#!" className="text-white">
-                  Link 1
+                <Link to="/" className="text-white">
+                  Home
                 </Link>
               </li>
               <li>
-                <Link href="#!" className="text-white">
-                  Link 2
+                <Link to="/About" className="text-white">
+About </Link>
+              </li>
+              <li>
+                <Link to="/Contact" className="text-white">
+                  Contact
                 </Link>
               </li>
               <li>
-                <a href="#!" className="text-white">
-                  Link 3
-                </a>
-              </li>
-              <li>
-                <a href="#!" className="text-white">
-                  Link 4
-                </a>
+                <Link to="/blog" className="text-white">
+Blog                </Link>
               </li>
             </ul>
           </MDBCol>
 
           <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
-            <h5 className="text-uppercase mb-0">Links</h5>
+            <h5 className="text-uppercase mb-0">Social Media</h5>
 
             <ul className="list-unstyled">
               <li>
-                <a href="#!" className="text-white">
-                  Link 1
-                </a>
+              <a href={'/https://www.facebook.com/HAYYA-107115075582816'} style={{fontSize:"17px"}}> 
+                                    < GrFacebookOption/> </a>
               </li>
               <li>
-                <a href="#!" className="text-white">
-                  Link 2
-                </a>
+              <a href={'https://www.linkedin.com/company/hayya/?viewAsMember=true'}style={{fontSize:"17px"}}>
+                                    < AiFillLinkedin /></a>
               </li>
               <li>
-                <a href="#!" className="text-white">
-                  Link 3
-                </a>
+              <a href={'https://twitter.com/jardat_rama'}style={{fontSize:"17px"}}>
+                                    <AiFillTwitterCircle /> </a>
               </li>
               <li>
-                <a href="#!" className="text-white">
-                  Link 4
-                </a>
+              <a href={'https://www.youtube.com/@hayya3947'}style={{fontSize:"17px"}}>
+       <AiOutlineYoutube /></a>
               </li>
             </ul>
           </MDBCol>
         </MDBRow>
+   
       </MDBContainer>
 
       <div
         className="text-center p-3"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
       >
-        © 2020 Copyright:
-        <a className="text-white" href="https://mdbootstrap.com/">
-          MDBootstrap.com
-        </a>
+        © 2020 Copyright:Hayya    
+       
       </div>
     </MDBFooter>
   );
